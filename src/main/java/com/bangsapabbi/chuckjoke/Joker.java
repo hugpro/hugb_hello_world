76bd9d869b0837c72433543b9a7e5bc315e16126
Skuli Arnlaugsson <skuli.arnlaugsson@gmail.com>
2015-10-18 21:25:29 +0000

    Step 5: Change to web service

--- src/main/java/com/bangsapabbi/chuckjoke/Joker.java
+++ src/main/java/com/bangsapabbi/chuckjoke/Joker.java
@@ -1,8 +1,14 @@
 package com.bangsapabbi.chuckjoke;
 
+import static spark.Spark.*;
+
 public class Joker {
     public static void main(String[] args) {
+        String port = System.getenv("PORT");
+        if (port != null) {
+            setPort(Integer.valueOf(port));
+        }
 	ChuckJoke chuck = new ChuckJoke();
-    	System.out.println(chuck.random());
+        get("/random", (req, res) -> chuck.random());
     }
 }
